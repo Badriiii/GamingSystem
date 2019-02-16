0786ada874855bc04abc2b50d5869e0ebb8eac67
Badri <badri.py@outlook.com>
2019-02-16 22:55:43 +0530

    update: update the blog admin component to use the update/delete methods created by service

--- src/app/admin/blogAdmin/blog-admin.component.ts
+++ src/app/admin/blogAdmin/blog-admin.component.ts
@@ -1,47 +1,75 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { UserService } from '../adminShared/user.service';
-import { BlogAdminService } from '../adminShared/blog-admin.service';
+import { Router } from '@angular/router';
 import * as firebase from 'firebase';
+import { BlogAdminService } from '../adminShared/blog-admin.service';
 import { Blog } from '../adminShared/blog';
 
 @Component({
-    templateUrl: './blog-admin.component.html',
-    styleUrls: ['./blog-admin.component.css']
-   })
+  templateUrl: './blog-admin.component.html',
+  styleUrls: ['./blog-admin.component.css']
+})
 
-export class BlogAdminComponent implements OnInit{
-
+export class BlogAdminComponent implements OnInit {
     theUser: string;
-    menuChoices: string;
+    menuChoice: string;
     blogPosts: Blog[];
+    formDisplay: boolean = true;
+    singlePost: Blog;
 
-    constructor(
-         private userSVC: UserService,
-         private blogAdminSVC: BlogAdminService, 
-         private router: Router)
-         { }
+    constructor( 
+        private userSVC: UserService, 
+        private router: Router, 
+        private blogAdminSVC: BlogAdminService 
+    ){}
 
-         ngOnInit(){
-            this.theUser = this.userSVC.loggedInUser;
-            this.getPosts();
-          }
-        
-          logout(){
-            this.userSVC.logout();
-            this.router.navigate(['']);
-          }
+    logout(){
+        this.userSVC.logout();
+        this.router.navigate(['']);
+    }
 
-          chooseMode(mode: string){
-              this.menuChoices = mode;
-          }
+    chooseMode(mode: string){
+        this.menuChoice = mode;
+    }
 
-          getPosts(){
-              let dbRef = firebase.database().ref('blogPosts/');
-              dbRef.once('value')
-              .then((snapshot) => {
-                  let tmp: string[] = snapshot.val();
-                  this.blogPosts = Object.keys(tmp).map(key => tmp[key])
-              });
-          }
+    ngOnInit(){
+        this.theUser = this.userSVC.loggedInUser;
+        this.getPosts();
+    }
+
+    getPosts(){
+        let dbRef = firebase.database().ref('blogPosts/');
+        dbRef.once('value')
+            .then((snapshot)=> {
+                let tmp: string[] = snapshot.val();
+                this.blogPosts = Object.keys(tmp).map(key => tmp[key])
+            });
+    }
+
+    editPost(thePost: Blog){
+        this.singlePost = thePost;
+        this.formDisplay = false;
+    }
+
+    cancelEdit(){
+        this.formDisplay = true;
+    }
+
+    updatePost(single: Blog){
+        this.blogAdminSVC.editPost(single);
+        this.formDisplay = true;
+    }
+
+    deletePost(single: Blog){
+        let verify = confirm(`Are you sure you want to delete this post?`)
+        if (verify == true) {
+            this.blogAdminSVC.removePost(single);
+            this.router.navigate(['/admin/']);
+        } else {
+            alert('Nothing deleted!');
+        }
+    }
+
+
+     
 }
