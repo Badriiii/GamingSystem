542f503357852c98fd80431e655e9d71a8f804be
Badri <badri.py@outlook.com>
2019-02-18 18:52:10 +0530

    feat: update the app to show the blog posts to guest users in home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,10 @@ import { AppComponent } from './start/app.component';
 import { NavComponent } from './shared/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { ErrorComponent } from './error/error.component';
+
 import { AppRoutingModule } from './shared/app.routing';
-import { AdminModule } from './admin/admin.module';
+import { AdminModule }  from './admin/admin.module';
+
 
 @NgModule({
     imports: [
